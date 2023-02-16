13a2d42799cde9f25079d3651bee590f2d10b6d6
mvoagency <dev@mvo.agency>
2023-02-16 19:03:42 +0530

    kaalan

---
+++ src/main/java/com/thouseef/kaalan/KaalanApplication.java
@@ -0,0 +1,14 @@
+package com.thouseef.kaalan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaalanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaalanApplication.class, args);
+		//kaalan saapda polama
+	}
+
+}
---
+++ src/test/java/com/thouseef/kaalan/KaalanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thouseef.kaalan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaalanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
